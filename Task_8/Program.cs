7c5f079dd7519f48e78a0f168a012cb63879540d
Dmitriy <potapidze.mos@mail.ru>
2022-10-05 00:56:41 +0300

    Добавил предварительное решение задачи 8

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -5,3 +5,33 @@
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int counter = 1;
+// while (counter <= number)
+// {
+//     Console.Write($"{counter} ");
+//     counter++;
+// }
+
+int counter = 1;
+bool not = true;
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (counter <= number)
+{
+    if (counter % 2 != 1)
+    {
+        Console.Write($"{counter} ");
+        not = false;
+    }
+    counter++;
+}
+if (not)
+{
+    Console.WriteLine("Введите натуральное число! ");
+}
