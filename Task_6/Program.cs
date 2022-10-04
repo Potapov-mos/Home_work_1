ad89b29673638de3497ddf1a76abf360187a9d77
Dmitriy <potapidze.mos@mail.ru>
2022-10-04 19:13:17 +0300

    Добавил решение задачи 6

--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -6,3 +6,16 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+
+Console.Write("Введите первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"{number} -> да");
+}
+else
+{
+    Console.WriteLine($"{number} -> нет");
+}
