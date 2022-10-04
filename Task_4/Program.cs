3086a0fe337f6ccb2a830df66688c30dadbfa4a1
Dmitriy <potapidze.mos@mail.ru>
2022-10-04 19:28:43 +0300

    Добавил решение задачи 4

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -6,3 +6,19 @@
 // 2, 3, 7 -> 7
 // 44, 5, 78 -> 78
 // 22, 3, 9 -> 22
+
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (max < a) max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+
+Console.WriteLine($"Максимальное число -> {max}");
