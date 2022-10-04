e904de706e4e6b7d43ac88fc07dc3d0310986e11
Dmitriy <potapidze.mos@mail.ru>
2022-10-04 18:20:27 +0300

    Решил задачу 2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -6,3 +6,22 @@
 // a = 5; b = 7 -> max = 7
 // a = 2; b = 10 -> max = 10
 // a = -9; b = -3 -> max = -3
+
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"{a} > {b}"); 
+}
+else if (a == b)
+{
+    Console.WriteLine("Равнозначные числа");
+}
+else 
+{
+    Console.WriteLine($"{a} < {b}");
+}
